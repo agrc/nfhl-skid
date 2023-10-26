d0ddd21051f6c62aeae7e2942e6499f4958be26a
Jake Adams <jdadams@utah.gov>
2023-10-26 11:02:01 -0600

    ci: secrets, dep handling

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     keywords=['gis'],
     install_requires=[
         # 'arcgis==2.1.*',
-        'ugrc-palletjack==4.*',
+        'ugrc-palletjack[nopro]==4.*',
         'agrc-supervisor==3.0.*',
     ],
     extras_require={
