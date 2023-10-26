6568e2abd586aca79ac8a2ae37e5ab6aa073c42d
Jake Adams <jdadams@utah.gov>
2023-10-26 11:02:02 -0600

    chore: clean up cloud function main()

--- src/nfhl/main.py
+++ src/nfhl/main.py
@@ -284,36 +284,5 @@ def process():  # pylint: disable=too-many-locals
         _remove_log_file_handlers(log_name, loggers)
 
 
-def main(event, context):  # pylint: disable=unused-argument
-    """Entry point for Google Cloud Function triggered by pub/sub event
-
-    Args:
-         event (dict):  The dictionary with data specific to this type of
-                        event. The `@type` field maps to
-                         `type.googleapis.com/google.pubsub.v1.PubsubMessage`.
-                        The `data` field maps to the PubsubMessage data
-                        in a base64-encoded string. The `attributes` field maps
-                        to the PubsubMessage attributes if any is present.
-         context (google.cloud.functions.Context): Metadata of triggering event
-                        including `event_id` which maps to the PubsubMessage
-                        messageId, `timestamp` which maps to the PubsubMessage
-                        publishTime, `event_type` which maps to
-                        `google.pubsub.topic.publish`, and `resource` which is
-                        a dictionary that describes the service API endpoint
-                        pubsub.googleapis.com, the triggering topic's name, and
-                        the triggering event type
-                        `type.googleapis.com/google.pubsub.v1.PubsubMessage`.
-    Returns:
-        None. The output is written to Cloud Logging.
-    """
-
-    #: This function must be called 'main' to act as the Google Cloud Function entry point. It must accept the two
-    #: arguments listed, but doesn't have to do anything with them (I haven't used them in anything yet).
-
-    #: Call process() and any other functions you want to be run as part of the skid here.
-    process()
-
-
-#: Putting this here means you can call the file via `python main.py` and it will run. Useful for pre-GCF testing.
 if __name__ == '__main__':
     process()
