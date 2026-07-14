4dc9d8104c94719e37fb5cf2d2b59b51f01bb786
stdavis <stdavis@utah.gov>
2026-07-14 08:28:57 -0600

    fix: fix hostname when running on GCP

--- src/nfhl/config.py
+++ src/nfhl/config.py
@@ -4,14 +4,28 @@ config.py: Configuration values. Secrets to be handled with Secrets Manager
 
 import logging
 import socket
+import urllib
 
 SKID_NAME = "nfhl_skid"
 
 AGOL_ORG = "https://utah-em.maps.arcgis.com"
+
+try:
+    url = "http://metadata.google.internal/computeMetadata/v1/project/project-id"
+    req = urllib.request.Request(url)
+    req.add_header("Metadata-Flavor", "Google")
+    with urllib.request.urlopen(req, timeout=15) as response:
+        project_id = response.read().decode()
+        if not project_id:
+            raise ValueError
+        HOST_NAME = project_id
+except Exception:
+    HOST_NAME = socket.gethostname()
+
 SENDGRID_SETTINGS = {  #: Settings for SendGridHandler
     "from_address": "noreply@utah.gov",
     "to_addresses": ["ugrc-developers@utah.gov", "hstrand@utah.gov"],
-    "prefix": f"{SKID_NAME} on {socket.gethostname()}: ",
+    "prefix": f"{SKID_NAME} on {HOST_NAME}: ",
 }
 LOG_LEVEL = logging.INFO
 LOG_FILE_NAME = "log"
